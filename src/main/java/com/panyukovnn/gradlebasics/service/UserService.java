c12fc16e76599b14d0c9fa642883212a9eaf10f4
Nickolay <zylex77@gmail.com>
2021-11-28 18:03:40 +0300

    prepare for video

---
+++ src/main/java/com/panyukovnn/gradlebasics/service/UserService.java
@@ -0,0 +1,14 @@
+package com.panyukovnn.gradlebasics.service;
+
+import com.panyukovnn.gradlebasics.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void process() {
+        User user = new User("Unknown");
+
+        user.setId("John");
+    }
+}
